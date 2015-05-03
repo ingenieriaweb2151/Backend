5a0614a8613178c6606178f10dc4f58cef2d110a
pahoalapizco <pahoc.alapizco@gmail.com>
2015-05-03 15:10:43 -0600

    Funcion GuardaEmp(), para registrar una nueva empresa

--- public/data/funs.php
+++ public/data/funs.php
@@ -36,29 +36,6 @@ function ValidaEntrada()
 
 }
 
-//Funcion para mostrar los proyectos registrados  (NO FUNCIONA u.u)
-/*function traeBanco()
-{
-	$respuesta   = false;
-	$conexion    = conectaBD();
-	$consulta    = sprintf("select * from bancoproy");
-	$resconsulta = mysql_query($consulta);
-	$renglones   = "";
-	while ($registro = mysql_fetch_array($resconsulta))
-	{
-		$respuesta = true;
-		$renglones.= "<tr>";
-		$renglones.= "<td>".$registro["usuario"]."</td>";
-		$renglones.= "<td>".$registro["nombre"]."</td>";
-		$renglones.= "<td>".$registro["apellido"]."</td>";
-		$renglones.= "<td>".$registro["estatus"]."</td>";
-		$renglones.= "</tr>";
-	}
-	$salidaJSON = array('respuesta' => $respuesta,
-						'renglones' => $renglones);
-	print json_encode($salidaJSON);
-}*/
-//Sección de opciones para elegir la funcion correspondiente que pide el .js
 $opcion =  $_POST ["opc"];
 switch ($opcion) 
 {
@@ -66,8 +43,8 @@ switch ($opcion)
 		 ValidaEntrada();
 	
 		break;
-	case 'traeBanco':
-		traeBanco();
+	case 'GuardaEmp':
+		GuardaEmp();
 	default:
 		# code...
 		break;
