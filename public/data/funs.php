f70869947e3ffad8029f5dcd2959c6159a0e6d6e
pahoalapizco <pahoc.alapizco@gmail.com>
2015-05-03 17:10:32 -0600

    Funcion GuardaEmp()

--- public/data/funs.php
+++ public/data/funs.php
@@ -36,6 +36,32 @@ function ValidaEntrada()
 
 }
 
+function GuardaEmp()
+{
+		$conexion = conectaBDpersonal('divestpro');
+		$res = false; 
+		$id = rand();//Elegimos un numero aleatorio para asignarlo como cveempr 
+		$ne = GetSQLValueString($_POST["nomemp"],"text");
+		$di = GetSQLValueString($_POST["dir"],"text");
+		$co = GetSQLValueString($_POST["col"],"text");
+		$ci = GetSQLValueString($_POST["ciu"],"text");
+		$cp = GetSQLValueString($_POST["cp"],"text");
+		$te = GetSQLValueString($_POST["tel"],"text");
+		$en = GetSQLValueString($_POST["enc"],"text");
+		$pu = GetSQLValueString($_POST["pues"],"text");
+		
+		$consultaInsert = "INSERT INTO  empresas(cveempr, nombre, domicil, colonia, ciudad, cp, telef, nomtitu, puetitu)
+                  			   VALUES ('$id','$ne','$di','$co','$ci','$cp','$te','$en','$pu')";
+   		 $resultadoInsert = mysql_query($consultaInsert);
+		if(mysql_affected_rows()>0){
+			$res = true;
+
+		}
+		
+		$salidaJSON = array('respuesta' => $respuesta);
+		print json_encode($salidaJSON);			
+}
+
 $opcion =  $_POST ["opc"];
 switch ($opcion) 
 {
