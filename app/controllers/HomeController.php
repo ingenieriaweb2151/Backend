0a6936d9dfe31c98280dea316b555f4d4da46b4b
pahoalapizco <pahoc.alapizco@gmail.com>
2015-05-04 09:50:24 -0600

    cambio de proyecto

--- app/controllers/HomeController.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class HomeController extends BaseController {
-    public function index()
-    {
-        return View::make('index');
-    }
-
-}
--- app/views/home.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.master')
-@section('content')
-
-<h1>ESTO Ya FUNCIONO</h1>
-<h2>OH SI FUNCIONO :D</h2>
-@stop
