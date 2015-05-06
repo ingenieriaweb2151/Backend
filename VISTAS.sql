63b7feb9c68f6e0b7cefef55f443626287a0cd30
pahoc.alapizco@gmail.com <pahoc.alapizco@gmail.com>
2015-05-05 21:22:52 -0600

    Vista proyAsignado

---
+++ VISTAS.sql
@@ -0,0 +1,34 @@
+-- VISTAS:
+
+-- Vista para el Banco de Proyectos
+
+use residencias;
+
+-- Vista para ver el banco de proyectos
+ create view BancoProy as (
+ select p.cveproy, e.nombre as 'Nombre de la empresa', e.domicil as 'Domicilio', e.telef as 'Telefono', p.nomresp as 'Nombre del responsable', p.nombre as 'Nombre del proyecto',c.carnom as 'Carrera', p.numresi as 'cupos'
+ from empresas e, proyectos p, dcarre c
+ where e.cveempr = p.cveempr and p.carcve = c.carcve);
+ 
+
+ -- Vista para buscar a los alumnos que cargaron recidencias 
+ create view buscarmat as (
+ select dl.aluctr  
+ from dlista dl, dalumn da, dmater dm
+ where dl.aluctr=da.aluctr and dl.matcve = dm.matcve and dm.matnom like '%RESIDEN%');
+ 
+ -- Vista para ver las solicitudes pendientes
+ create view solPendientes as(
+ Select a.aluctr, a.alunom, a.aluapp as apealumn, a.aluapm, p.nombre as nombreproy,
+ p.cveproy, e.nombre as nombreempr, e.cveempr,s.pdocve from dalumn a, proyectos p, 
+ solicitudes s, empresas e WHERE a.aluctr = s.aluctr and p.cveproy = s.cveproy and 
+ e.cveempr = p.cveempr);
+ 
+ -- VISTA DE LOS PROYECTOS ASIGNADOS AL ALUMNO Y ASESOR
+ 
+ create view proyAsignado as(
+    Select a.aluctr, a.alunom, a.aluapp as apealumn, a.aluapm, 
+    p.nombre as nombreproy, p.cveproy, e.nombre as nombreempr, e.cveempr, dp.pernom, 
+    dp.perape, dp.percve 
+    from dalumn a, proyectos p,asignproyectos ap, empresas e, dperso dp, asignaseinternos ai
+    WHERE a.aluctr = ap.aluctr and p.cveproy = ap.cveproy and e.cveempr = ap.cveempr and ai.percve = dp.percve);
