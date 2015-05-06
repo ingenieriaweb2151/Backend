479dceeacdc75a6c18358782599dcb4231e5b926
pahoc.alapizco@gmail.com <pahoc.alapizco@gmail.com>
2015-05-06 02:07:48 -0600

    Modificacion de la vista proyAsignado

--- VISTAS.sql
+++ VISTAS.sql
@@ -27,12 +27,10 @@ use residencias;
  -- VISTA DE LOS PROYECTOS ASIGNADOS AL ALUMNO Y ASESOR
  
  create view proyAsignado as(
-    Select a.aluctr, a.alunom, a.aluapp as apealumn, a.aluapm, 
-    p.nombre as nombreproy, p.cveproy, e.nombre as nombreempr, e.cveempr, dp.pernom, 
-    dp.perape, dp.percve 
-    from dalumn a, proyectos p,asignproyectos ap, empresas e, dperso dp, asignaseinternos ai
-    WHERE a.aluctr = ap.aluctr and p.cveproy = ap.cveproy and e.cveempr = ap.cveempr and ai.percve = dp.percve);
-    
+ Select ap.aluctr, a.alunom, a.aluapp as apealumn, a.aluapm, p.nombre as nombreproy, p.cveproy, e.nombre as nombreempr, e.cveempr, dp.pernom, dp.perape, dp.percve 
+ from dalumn a, proyectos p,asignproyectos ap, empresas e, dperso dp, asignaseinternos ai 
+ WHERE a.aluctr = ap.aluctr and p.cveproy = ap.cveproy and ai.cveproy = p.cveproy and e.cveempr = ap.cveempr and ai.percve = dp.percve);
+ 
 create view buscarAsesores as(
 SELECT s.aluctr, dc.carcve, dp.percve, dcar.depcve, dcar.carnom, dp.pernom, dp.perape 
 FROM solicitudes s, dcarre dcar, dperso dp, dcalum dc 
